5971dbe0ff461ad765aa8a0c1b607530a8117283
Wendel <wendel4019@gmail.com>
2024-03-14 19:12:12 -0300

    closes #3 - Implement the sum button with JS

--- script.js
+++
@@ -1,8 +0,0 @@
-function calc() {
-    var num1 = parseFloat(document.getElementById("num1").value);
-    var num2 = parseFloat(document.getElementById("num2").value);
-
-    var output = num1 + num2;
-
-    document.getElementById("output").value = output;
-}
