0468768f677dd92892045905b87b542f9efbcfad
Wendel <wendel4019@gmail.com>
2024-03-15 18:39:14 -0300

    closes #5, closes #6 - History

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ function calc(op) {
    switch(op) {
         case '+' : output = num1 + num2; break;
         case '-' : output = num1 - num2; break;
-        case '*' : output = num1 * num2; break;
+        case 'x' : output = num1 * num2; break;
         case '/' : output = num1 / num2; break;
    }
     
     document.getElementById("output").value = output;
+
+    var history = document.getElementById("history");
+    var newHispory = "<p>" + num1 + " " + op + " " + num2 + " = " + output + "</p>";
+
+    history.innerHTML = newHispory + history.innerHTML;
+
+    if(history.childNodes.length > 10) {
+        history.removeChild(history.childNodes[10]);
+    }
+
 }
